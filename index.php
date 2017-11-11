35b617749a7e284e41c2048c5fbe77db993d9143
Gary Kovar <gkovar@gmail.com>
2017-11-11 13:12:20 -0500

    adds  X-CSRF-Token

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use Pimple;
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token,  X-CSRF-Token');
 
 require_once ( __DIR__ ) . '/vendor/autoload.php';
 
